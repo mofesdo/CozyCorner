cc7b1a6c16fce51843c2a673947d8b734bc82806
Parnika006 <parnikasingh006@gmail.com>
2025-04-18 15:48:18 -0700

    waterfall sound final check update

--- components/waterfall.js
+++ components/waterfall.js
@@ -1,5 +1,7 @@
-const waterfallImg = document.querySelector("#waterfall-img");
+/* const waterfallImg = document.querySelector("#waterfall-img");
 const audio = document.querySelector("#waterfall-audio");
+
+export default class waterfall {}
 
 waterfallImg.addEventListener("mouseenter", () => {
   audio.currentTime = 0; //resets the audio
@@ -8,4 +10,28 @@ waterfallImg.addEventListener("mouseenter", () => {
 
 waterfallImg.addEventListener("mouseleave", () => {
   audio.pause();
-});
+}); */
+
+export class WaterfallSound {
+  constructor(imageSelector, audioSelector) {
+    this.image = document.querySelector(imageSelector);
+    this.audio = document.querySelector(audioSelector);
+
+    if (this.image && this.audio) {
+      this.init();
+    } else {
+      console.error("WaterfallSound: Could not find image or audio element.");
+    }
+  }
+
+  init() {
+    this.image.addEventListener("mouseenter", () => {
+      this.audio.currentTime = 0;
+      this.audio.play();
+    });
+
+    this.image.addEventListener("mouseleave", () => {
+      this.audio.pause();
+    });
+  }
+}
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("linked");
+import { WaterfallSound } from "./components/waterfall.js";
+
+const waterfall = new WaterfallSound("#waterfall-img", "#waterfall-audio");
